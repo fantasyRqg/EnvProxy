dafcb712b63153fe7fa5e13ca62ca33e3b72e753
Rqg <ranqingguo@youzan.com>
2018-05-04 14:58:17 +0800

    draw session data flow

--- app/src/main/cpp/session/Session.h
+++ app/src/main/cpp/session/Session.h
@@ -2,6 +2,37 @@
 // Created by Rqg on 24/04/2018.
 //
 
+/**
+ *
+ *                    Tun                                            socket
+ *                     /\                                               /\
+ *                    /  \                                             /  \
+ *                   /    \                                           /    \
+ *                  /      \                                         /      \
+ *                 ∨        \                                       ∨        \
+ *           onTunDown    onSockUp                           onSocketDown  onTunUp
+ *               |           ∧                                    |           ∧
+ *               ∨           |                                    ∨           |
+ *          --------------------------------------------------------------------------------
+ *         |      session  1                                                                |
+ *          --------------------------------------------------------------------------------
+ *               |           ∧                                    |           ∧
+ *               |           |                                    |           |
+ *           onTunDown    onSockUp                           onSocketDown  onTunUp
+ *               |           |                                    |           |
+ *               ∨           |                                    ∨           |
+ *          --------------------------------------------------------------------------------
+ *         |      session   2                                                               |
+ *          --------------------------------------------------------------------------------
+ *               |           ∧                                    |           ∧
+ *               |           |          send data to tun          |           |
+ *               |           |-------------------<----------------∨           |
+ *               |                                                            |
+ *               ∨----------------------------->-------------------------------
+ *                                      send data to socket
+ *
+ */
+
 #ifndef ENVPROXY_TASK_H
 #define ENVPROXY_TASK_H
 
@@ -13,6 +44,7 @@ struct SessionInfo;
 
 class TransportPkt;
 
+struct ProxyContext;
 
 class Session {
 public:
@@ -20,7 +52,37 @@ public:
 
     virtual ~Session();
 
-//    virtual int onTunDown(uint8_t *data);
+    /**
+     * data flow tun -> socket
+     * @param ctx
+     * @param data
+     * @return
+     */
+    virtual uint8_t *onTunDown(ProxyContext *ctx, uint8_t *data);
+
+    /**
+     *
+     * @param ctx
+     * @param data
+     * @return
+     */
+    virtual uint8_t *onTunUp(ProxyContext *ctx, uint8_t *data);
+
+    /**
+     *
+     * @param ctx
+     * @param data
+     * @return
+     */
+    virtual uint8_t *onSocketDown(ProxyContext *ctx, uint8_t *data);
+
+    /**
+     *
+     * @param ctx
+     * @param data
+     * @return
+     */
+    virtual uint8_t *onSocketUp(ProxyContext *ctx, uint8_t *data);
 
 
 public:
